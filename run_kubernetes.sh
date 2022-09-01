45f3716ce6e842c9178f4c6092b07d06447d1694
ModibboMuawiyya <modibbomm@gmail.com>
2022-09-01 02:26:31 +0100

    first commit

---
+++ run_kubernetes.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath="muawiyya/houseproj:v1"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run houseproj --image=muawiyya/houseproj:v1 --port=80 --labels app=houseproj
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward houseproj 8000:80
